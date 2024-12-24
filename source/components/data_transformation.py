24af88b6f79d819626f734d191a28b3b3667b75a
ravi0dubey <ravi0dubey@gmail.com>
2024-12-24 08:21:03 -0500

    changes done

---
+++ source/components/data_transformation.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+from pathlob import Path
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+from source.logger.logger import logging    
+from source.exception.exception import customexception
+
+@dataclass
+class DataTransformationConfig:
+    def __init__(self):
+        pass
+
+class DataTransformation:
+    def __init__(self):
+        pass
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise customexception(e,sys)
