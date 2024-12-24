e38b7db00c829b9b185ecb277e594b16c02123c4
ravi0dubey <ravi0dubey@gmail.com>
2024-12-24 17:52:18 -0500

    Revert "Revert "MODEL TRAINING AND EVALUATION done""
    This reverts commit 26c6b0dbba7902569ea05c98de3c790b0571577c.

--- source/utils/utils.py
+++ source/utils/utils.py
@@ -15,6 +15,15 @@ def save_object(file_path, obj):
     except Exception as e:
         raise customexception(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise customexception(e,sys)
+    
+
 def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
@@ -31,12 +40,32 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
         logging.info('Exception occured during model training')
         raise customexception(e,sys)
     
-def load_object(file_path):
-    try:
-        with open(file_path,'rb') as file_obj:
-            return pickle.load(file_obj)
-    except Exception as e:
-        logging.info('Exception Occured in load_object function utils')
-        raise customexception(e,sys)
 
-    
+
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise customexception(e, sys) from e
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise customexception(e, sys) from e 
